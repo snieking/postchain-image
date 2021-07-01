047cb0e67387a21da8a6271a4345841cf95ac7f2
Viktor Plane <viktor.plane@chromaway.com>
2021-07-01 13:09:53 +0200

    Add support for custom ports

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export PGPORT=$(cat /usr/src/rell/config/node-config.properties | grep "^database.url=" | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')
+
+
 postgres-entrypoint.sh postgres &
 
 
