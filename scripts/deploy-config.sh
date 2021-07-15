3a9b011fc92ee83940cc9ffb2be1be7a150c370a
Viktor Plane <viktor.plane@chromaway.com>
2021-07-15 18:46:19 +0200

    Handle different ports of postgres

--- scripts/deploy-config.sh
+++ scripts/deploy-config.sh
@@ -13,7 +13,7 @@ SCHEMA=$(get_value_from_properties_file /usr/src/rell/config/node-config.propert
 CHAIN_ID=$(xmllint --xpath 'string(//run/chains/chain/@iid)' /usr/src/rell/$RUN_XML)
 
 BLOCKS_TABLE="c${CHAIN_ID}.blocks"
-BLOCK_HEIGHT=$(psql -qtAX --username "$POSTGRES_USER" --no-password -c "SET search_path to $SCHEMA; SELECT block_height FROM \"$BLOCKS_TABLE\" ORDER BY block_height DESC LIMIT 1;" $POSTGRES_DB)
+BLOCK_HEIGHT=$(psql -qtAX -p $PORT --username "$POSTGRES_USER" --no-password -c "SET search_path to $SCHEMA; SELECT block_height FROM \"$BLOCKS_TABLE\" ORDER BY block_height DESC LIMIT 1;" $POSTGRES_DB)
 DEPLOY_HEIGHT=$((BLOCK_HEIGHT + 5))
 
 echo "Current block height is $BLOCK_HEIGHT, new config will be deployed at block height $DEPLOY_HEIGHT"
